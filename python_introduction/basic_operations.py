8fb542c978189f072637f39986779a527474141d
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2024-11-30 21:23:29 +0200

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+# Define the two numbers
+number1 = 10
+number2 = 5
+
+# Perform the arithmetic operations and print the results
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
